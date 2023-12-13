79b5a80c66a8abc950219b49f723455c4e6b1278
hcbryant9 <bryanthc@vcu.edu>
2023-12-12 19:49:06 -0500

    fixing paper interaction

--- Assets/FilingCabinet.cs
+++ Assets/FilingCabinet.cs
@@ -8,13 +8,22 @@ public class FilingCabinet : MonoBehaviour
 
     [SerializeField] private Realtime realtime;
     public TrainSceneManager trainSceneManager;
+    public AudioController audioController;
 
+    public GameObject paper;
+
+    private void Start()
+    {
+        paper.SetActive(false);
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Hand"))
         {
             if (realtime.clientID == 0)
             {
+                paper.SetActive(true);
+                audioController.PlayHeadphones();
                 trainSceneManager.johnDialouge1();
             }
         }
